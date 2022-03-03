8db139a7b762bc8bb92f0d074b290f67dcd8a4c0
Patrick Innes <2523573i@student.gla.ac.uk>
2022-03-03 20:23:00 +0000

    More Tweaks

--- Quizker_Project/Quizker/models.py
+++ Quizker_Project/Quizker/models.py
@@ -5,33 +5,51 @@ from django.db import models
 class Category(models.Model):
         Title = models.CharField(max_length=128, unique=True)
         Description = models.CharField(max_length=256)
-        
+        def __str__(self):
+             return self.Title 
+        class Meta:
+            verbose_name_plural = "Categories"
+
 class Quiz(models.Model):
         Title = models.CharField(max_length = 128,unique=True)
         #Creator = models.ForeignKey(User, on_delete=models.CASCADE)
         Category = models.ForeignKey(Category, on_delete=models.CASCADE)
         Date = models.DateField()
         Description = models.CharField(max_length=256)
+        def __str__(self):
+             return self.Title
+        class Meta:
+            verbose_name_plural = "Quizzes"
 class Question(models.Model):
         QuestionID = models.IntegerField(unique=True)
         Quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
-        Image = models.ImageField()
+        Image = models.ImageField(blank=True)
         Text = models.CharField(max_length=256)
+        def __str__(self):
+             return self.Quiz.Title + str(self.QuestionID)
 class TrueOrFalse(Question):
         Answer = models.BooleanField()
         def correctAnswer(self,attempt):
                return Answer==attempt
+        class Meta:
+            verbose_name_plural = "True or False Questions"
 class OpenEnded(Question):
         Answer = models.CharField(max_length=128)
         def correctAnswer(self,attempt):
               return Answer == attempt
+        class Meta:
+            verbose_name_plural = "Open ended Questions"
 class MultipleChoice(Question):
        def correctAnswer(self,choice):
               return choice.Correct
+       class Meta:
+            verbose_name_plural = "Multiple Choice Questions"
+  
 class choice(models.Model):
         ChoiceID = models.IntegerField(unique=True)
         Question = models.ForeignKey(MultipleChoice,on_delete=models.CASCADE)
         Text = models.CharField(max_length=128)
         Correct = models.BooleanField(default=False)
-       
+        def __str__(self):
+            return ChoiceID
 
