eb8ee3f868487aba67ecf9648302e8678c7861b5
Patrick Innes <2523573i@student.gla.ac.uk>
2022-03-17 22:13:01 +0000

    Settings

--- Quizker_Project/Quizker_Project/urls.py
+++ Quizker_Project/Quizker_Project/urls.py
@@ -17,10 +17,13 @@ from django.contrib import admin
 from django.urls import path
 from Quizker import views
 from django.urls import include 
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('',views.Home,name="Home"),
     path('Quizker/',include('Quizker.urls')),
     path('accounts/', include('registration.backends.simple.urls')),
     path('admin/', admin.site.urls),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
