edbca431360c3ad1293cac70e07bc81a05dcf27d
Andres La Riva <2564664L@student.gla.ac.uk>
2022-03-28 17:29:40 +0100

    tests part 2

--- Quizker_Project/Quizker/tests.py
+++ Quizker_Project/Quizker/tests.py
@@ -1,9 +1,10 @@
 from django.test import TestCase
 from .models import Category, Quiz, Question, TrueOrFalse, OpenEnded, MultipleChoice, Choice, QuizAttempt
-from django.urls import reverse
+from django.urls import reverse,path
 import os
 import warnings
 import importlib
+from .views import *
 from django.conf import settings
 from django.contrib.auth.models import User
 from django.db.models.query import QuerySet
@@ -17,6 +18,20 @@ def add_quiz(cat,id,title,creator,date,description,QuestionType,likes= 0):
         quiz.save()
         return quiz
 
+def add_open_ended(id,quiz, image, text, answer):
+    qe = OpenEnded.objects.get_or_create(id = id, quiz=quiz, image=image,text = text, answer = answer)[0]
+    qe.save()
+    return qe
+
+def add_multiple_choice(id,quiz, image, text):
+    qe = MultipleChoice.objects.get_or_create(id = id, quiz=quiz, image=image,text = text)[0]
+    qe.save()
+    return qe
+
+def add_choice(id,question, text, correct):
+    ch = Choice.objects.get_or_create(id = id, question=question,text = text, correct = correct)[0]
+    ch.save()
+    return ch
 
 #Test Meta functions
 class CategoryMethodTests(TestCase):
@@ -26,18 +41,6 @@ class CategoryMethodTests(TestCase):
         self.assertEqual((category.title != ""), True)
 
 class QuizMethodTests(TestCase):
-
-    def test_ensure_id_unique(self):
-        category = Category(id='01', title="", description ="Teste stuff eh")
-        category.save()
-        the_user = User.objects.get_or_create(username="Za Warudo", password = 0)[0]
-        the_user.set_password("1234")
-        quiz = add_quiz(category,"01","Test Quiz",the_user,"2020-03-04","A lil test","OpenEnded",0)
-        quiz.save()
-        quiz2 = add_quiz(category,"01","Dummy Quiz",the_user,"2020-03-04","A lil testt","OpenEnded",0)
-        quiz2.save()
-        self.assertEqual((quiz.id != quiz2.id), True)
-        print(quiz.id != quiz2.id)
 
     def test_quiz_starts_0_likes(self):
         category = Category(id='01', title="", description ="Teste stuff eh")
@@ -70,14 +73,17 @@ class ContactUsViewTests(TestCase):
 
         response = self.client.get(reverse('Quizker:ContactUs'))
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response,
+        self.assertContains(response,"First Name")
+        self.assertContains(response,"Last Name")
+        self.assertContains(response,"Subject")
 
     def test_template_filename(self):
-        self.assertTemplateUsed(self.response, 'Quizker/Home.html', f"{FAILURE_HEADER}Wrong Template{FAILURE_FOOTER}")
+        response = self.client.get(reverse('Quizker:ContactUs'))
+        self.assertTemplateUsed(response, 'Quizker/ContactUs.html', f"{FAILURE_HEADER}Wrong Template{FAILURE_FOOTER}")
 
 class QuizzesViewTests(TestCase):
 
-    def set_up(self):
+    def setUp(self):
         self.response = self.client.get(reverse('Quizker:Quizzes'))
         self.content = self.response.content.decode()
 
@@ -98,8 +104,13 @@ class QuizzesViewTests(TestCase):
         self.assertTrue('Quizzes' in self.response.context, f"{FAILURE_HEADER}We couldn't find a 'categories' variable in the context dictionary within the index() view. Check the instructions in the book, and try again.{FAILURE_FOOTER}")
         self.assertEqual(type(self.response.context['Quizzes']), QuerySet, f"{FAILURE_HEADER}The 'categories' variable in the context dictionary for the index() view didn't return a QuerySet object as expected.{FAILURE_FOOTER}")
         self.assertEqual(expected_quiz_order, list(self.response.context['Quizzes']), f"{FAILURE_HEADER}Incorrect categories/category order returned from the index() view's context dictionary -- expected {expected_quiz_order}; got {list(self.response.context['Quizzes'])}.{FAILURE_FOOTER}")
+    
+    def test_template_filename(self):
+        response = self.client.get(reverse('Quizker:Quizzes'))
+        self.assertTemplateUsed(response, 'Quizker/Quizzes.html', f"{FAILURE_HEADER}Wrong Template{FAILURE_FOOTER}")
 
 class CreateQuizViewTests(TestCase):
+    
 
     def proper_quiz_creation_message(self):
         category = Category(id='01', title="", description ="Teste stuff eh")
@@ -111,7 +122,97 @@ class CreateQuizViewTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Congratulations, for you that is quiz number")
 
+    
+
 #for quiz attempt, points cant be negative, same as questions attempted
+class CreateQuestionViewTests(TestCase):
+
+
+    def remove_question_existence(self):
+        category = Category(id='01', title="", description ="Teste stuff eh")
+        category.save()
+        the_user = User.objects.get_or_create(username="Za Warudo", password = 0)[0]
+        the_user.set_password("1234")
+        add_quiz(category,"01","Test Quiz",the_user,"2020-03-04","A lil test","OpenEnded",0)
+        response = self.client.get(reverse('Quizker:CreateQuestion'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Remove")
+
+    def test_question_existence(self):
+        category = Category(id='01', title="", description ="Teste stuff eh")
+        category.save()
+        the_user = User.objects.get_or_create(username="Za Warudo", password = 0)[0]
+        the_user.set_password("1234")
+        the_quiz = add_quiz(category,"07","Test Multiple Choice Quiz",the_user,"2020-04-03","A lil test","MultipleChoice",0)
+        add_multiple_choice("01",the_quiz,None,"Which of this is a birb")
+        response = self.client.get(reverse("Quizker:CreateQuestion",kwargs={"quiz_title_slug" : the_quiz.slug}))
+        self.assertContains(response, "Which of this is a birb")
+
+class CreateChoiceViewTests(TestCase):
+
+
+    def test_choice_existence(self):
+        category = Category(id='01', title="", description ="Teste stuff eh")
+        category.save()
+        the_user = User.objects.get_or_create(username="Za Warudo", password = 0)[0]
+        the_user.set_password("1234")
+        the_quiz = add_quiz(category,"07","Test Multiple Choice Quiz",the_user,"2020-04-03","A lil test","MultipleChoice",0)
+        the_question = add_multiple_choice("01",the_quiz,None,"Which of this is a birb")
+        add_choice("01",the_question,"Dog",False)
+        add_choice("02",the_question,"Cat",False)
+        add_choice("03",the_question,"Eagle",True)
+        response = self.client.get(reverse("Quizker:CreateChoice"))
+        self.assertContains(response, "Dog")
+        self.assertContains(response, "Cat")
+        self.assertContains(response, "Eagle")
+
+    #def test_template_filename(self):
+        #response = self.client.get(reverse('Quizker:CreateChoice'))
+        #self.assertTemplateUsed(response, 'Quizker/CreateChoice.html', f"{FAILURE_HEADER}Wrong Template{FAILURE_FOOTER}")
+
+class ResultsViewTests(TestCase):
+    def test_result_message(self):
+
+        category = Category(id='01', title="", description ="Teste stuff eh")
+        category.save()
+        the_user = User.objects.get_or_create(username="Za Warudo", password = 0)[0]
+        the_user.set_password("1234")
+        the_quiz = add_quiz(category,"07","Test Multiple Choice Quiz",the_user,"2020-04-03","A lil test","MultipleChoice",0)
+        response = self.client.get(reverse('Quizker:Results'),kwargs={"quiz_title_slug" : the_quiz.slug})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Congratulations, your result is')
+
+class UserProfileViewTests(TestCase):
+
+    def setUp(self):
+        self.response = self.client.get(reverse('Quizker:UserProfile'))
+        self.content = self.response.content.decode()
+
+    def test_user_message(self):
+
+        response = self.client.get(reverse('Quizker:UserProfile'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Quizzes created")
+
+    def test_template_filename(self):
+        self.assertTemplateUsed(self.response, 'Quizker/UserProfile.html', f"{FAILURE_HEADER}Wrong Template{FAILURE_FOOTER}")
+
+class LeadearboardViewTests(TestCase):
+
+    def setUp(self):
+        self.response = self.client.get(reverse('Quizker:Leaderboard'))
+        self.content = self.response.content.decode()
+
+    #def test_user_message(self):
+
+        #response = self.client.get(reverse('Quizker:Home'))
+        #self.assertEqual(response.status_code, 200)
+        #self.assertContains(response, 'A platform to take quizzes in a number of categories, compete with others and even create your own for a fun evening with your friends or school-work.')
+
+    def test_template_filename(self):
+        self.assertTemplateUsed(self.response, 'Quizker/Leaderboard.html', f"{FAILURE_HEADER}Wrong Template{FAILURE_FOOTER}")
+
+
 class Chapter5PopulationScriptTests(TestCase):
 
     """
